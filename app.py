2b91a97be947771e0d7059d99d6151083b724888
Daniel Monteagudo <dmonte3210@gmail.com>
2016-09-21 13:08:04 -0400

    added templates for coolness

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,21 +7,29 @@ def index():
     return '''
 <a href="/">Stay here</a>
 <a href="/pagetwo">Go to page two</a>
-<a href="/pagethree">Go to page three</a>'''
+<a href="/pagethree">Go to page three</a>
+<a href="/mytemplate">Go to my render template</a>'''
 
 @app.route("/pagetwo")
 def pagetwo():
     return '''
-<a href="/">Go back to index</a>
+<a href="/">Go back to home</a>
 <a href="/pagetwo">Stay here</a>
 <a href="/pagethree">Go to page three</a>'''
 
 @app.route("/pagethree")
 def pagethree():
     return '''
-<a href="/">Go back to index</a>
+<a href="/">Go back to home</a>
 <a href="/pagetwo">Go to page two</a>
 <a href="/pagethree">Stay here</a>'''
 
+pals = ['Kelly', 'Sachal', 'Jason', 'Lawrence']
+
+@app.route("/mytemplate")
+def template():
+    return render_template("ninja.html", pals=pals, color="green")
+
 if __name__=="__main__":
+    app.debug = True
     app.run()
