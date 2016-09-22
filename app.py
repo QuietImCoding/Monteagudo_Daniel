f26459f710ecf5a8d527d5bafec7958330560006
Daniel Monteagudo <dmonte3210@gmail.com>
2016-09-22 13:04:02 -0400

    Did the tablify

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -24,12 +25,41 @@ def pagethree():
 <a href="/pagetwo">Go to page two</a>
 <a href="/pagethree">Stay here</a>'''
 
+
+file=open("occupations.csv", "r")
+stringthing = file.read()
+
+# Splits the string into an array called splitString
+splitString = str.split(stringthing, "\r\n")
+
+dict = {}
+# Loops through array line by line
+for line in splitString:
+	if "Total" not in line:
+		if len(line)>0 and line[0]=='"':
+			line = line[1:]
+			dict[float(line[line.index('"')+2:])]=line[0:line.index('"')]
+		elif len(line)>0 and splitString.index(line)!=0:
+			#print line
+			dict[float(line[line.index(',')+1:])]=line[0:line.index(',')]
+        
+
+def getRandomOccupation():
+	r = random.randint(0,997)
+	temp = 0 
+	for key in dict:
+		temp += 10 * key
+		if r < temp:
+			return dict[key]
+		if temp == 859:
+			return dict[key]
+
 pals = ['Kelly', 'Sachal', 'Jason', 'Lawrence']
 
-@app.route("/mytemplate")
-def template():
-    return render_template("ninja.html", pals=pals, color="green")
-
+@app.route("/occupations")
+def tablify():
+    return render_template("ninja.html", dict=dict, title="Occupations", occupation=getRandomOccupation())
+        
 if __name__=="__main__":
     app.debug = True
     app.run()
