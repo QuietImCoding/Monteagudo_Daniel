83395aa2c063c83d331dbbf11af39ad28f1ad066
Daniel Monteagudo <dmonte3210@gmail.com>
2016-09-30 13:08:30 -0400

    I DID SOME STUFF

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import random
+import random, hashlib
 
 app = Flask(__name__)
 
@@ -7,9 +7,27 @@ app = Flask(__name__)
 def login():
     return render_template("loginpage.html")
 
+@app.route("/create")
+def newAccount():
+    return render_template("makeaccount.html")
+
+@app.route("/createsuccess", methods=['POST'])
+def creationSuccess():
+    sentUser = request.form['username']
+    sentPass = request.form['pass']
+    file logins = open("demlogins.csv", "a")
+    logins.write(sentUser + ',' + sentPass)
+
 @app.route("/results", methods=['POST'])
 def results():
-    if request.form['username']=='breakerOfRepos' and request.form['pass']=='H1MrBr0wn!!':
+    file logins = open("demlogins.csv", "r")
+    
+    myHash = hashlib.sha1(request.form['pass'])
+    #myHash.hexdigest()#update(request.form['pass'])
+    sentUser = request.form['username']
+    sentPwd = request.form['pass']
+    
+    if sentUser=='breakerOfRepos' and myHash.hexdigest()==hashlib.sha1('H1MrBr0wn!!').hexdigest():
         return render_template("output.html", success=True)
     else:
         return render_template("output.html", success=False)
