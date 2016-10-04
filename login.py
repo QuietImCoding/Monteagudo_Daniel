0699b56fbacc96438a7a667dd9412439a8e5fec2
Daniel Monteagudo <dmonte3210@gmail.com>
2016-10-04 15:41:27 -0400

    did all the hash-slinging slasher stuff and account creation works (as long as username doesn't contain ,)

--- login.py
+++ login.py
@@ -15,19 +15,40 @@ def newAccount():
 def creationSuccess():
     sentUser = request.form['username']
     sentPass = request.form['pass']
-    file logins = open("demlogins.csv", "a")
-    logins.write(sentUser + ',' + sentPass)
+    sentPassConfirm = request.form['passconfirm']
+    if sentPass != sentPassConfirm:
+        return "<h1>Passwords did not match</h1><a href='/create'>Try again</a>"
+    if sentUser in parseCSV("demlogins.csv"):
+        return "<h1>Username in use</h1><a href='/create'>Try again</a>"
+    logins = open("demlogins.csv", "a")
+    logins.write(sentUser + ',' + hashlib.sha1(sentPass).hexdigest() + '\n')
+    return "<h4>ACCOUNT CREATED SUCCESSFULLY</h4>" + render_template("loginpage.html")
+
+def parseCSV(location):
+    loginstring = open("demlogins.csv", "r")
+    loginstring = loginstring.read()
+    loginpairs = loginstring.split("\n")
+    logins = {}
+    print "\n\n\n"
+    for pair in loginpairs:
+        if len(pair)>1:
+            logins[pair[0:pair.index(',')]] = pair[pair.index(",")+1:]
+    print "\n\n\n"
+    return logins
 
 @app.route("/results", methods=['POST'])
 def results():
-    file logins = open("demlogins.csv", "r")
+
+    logins = parseCSV("demlogins.csv")
+    #logins = open("demlogins.csv", "r")
     
     myHash = hashlib.sha1(request.form['pass'])
     #myHash.hexdigest()#update(request.form['pass'])
     sentUser = request.form['username']
     sentPwd = request.form['pass']
-    
-    if sentUser=='breakerOfRepos' and myHash.hexdigest()==hashlib.sha1('H1MrBr0wn!!').hexdigest():
+
+    if sentUser in logins and logins[sentUser] == myHash.hexdigest():
+    #if sentUser=='breakerOfRepos' and myHash.hexdigest()==hashlib.sha1('H1MrBr0wn!!').hexdigest():
         return render_template("output.html", success=True)
     else:
         return render_template("output.html", success=False)
