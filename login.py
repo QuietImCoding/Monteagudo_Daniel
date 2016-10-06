cb90b64da35a1678cb4fdacb363e66322d47484e
Daniel Monteagudo <dmonte3210@gmail.com>
2016-10-06 13:07:32 -0400

    made the sessions work

--- login.py
+++ login.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import random, hashlib
 
 app = Flask(__name__)
 
-@app.route("/")
-def login():
+@app.route("/", methods=['POST', 'GET'])
+def login():        
+    if request.form != None:
+        session.pop("user")
+    if "user" in session:
+        return redirect("/results")
     return render_template("loginpage.html")
 
+
+@app.route("/jacobo")
+def js():
+    return redirect("http://xkcd.com")
+    
 @app.route("/create")
 def newAccount():
     return render_template("makeaccount.html")
@@ -36,23 +45,26 @@ def parseCSV(location):
     print "\n\n\n"
     return logins
 
-@app.route("/results", methods=['POST'])
+@app.route("/results", methods=['GET', 'POST'])
 def results():
-
-    logins = parseCSV("demlogins.csv")
-    #logins = open("demlogins.csv", "r")
-    
-    myHash = hashlib.sha1(request.form['pass'])
-    #myHash.hexdigest()#update(request.form['pass'])
-    sentUser = request.form['username']
-    sentPwd = request.form['pass']
-
-    if sentUser in logins and logins[sentUser] == myHash.hexdigest():
-    #if sentUser=='breakerOfRepos' and myHash.hexdigest()==hashlib.sha1('H1MrBr0wn!!').hexdigest():
+    if "user" in session:
         return render_template("output.html", success=True)
     else:
-        return render_template("output.html", success=False)
-    
+        logins = parseCSV("demlogins.csv")
+        #logins = open("demlogins.csv", "r")
+        
+        myHash = hashlib.sha1(request.form['pass'])
+        #myHash.hexdigest()#update(request.form['pass'])
+        sentUser = request.form['username']
+        sentPwd = request.form['pass']
+        session["user"] = sentUser
+        if sentUser in logins and logins[sentUser] == myHash.hexdigest():
+            #if sentUser=='breakerOfRepos' and myHash.hexdigest()==hashlib.sha1('H1MrBr0wn!!').hexdigest():
+            return render_template("output.html", success=True)
+        else:
+            return render_template("output.html", success=False)
+        
 if __name__=="__main__":
     app.debug = True
+    app.secret_key = "I4m4H4ppyR0b0tW00pW00pW00pW00p"
     app.run()
